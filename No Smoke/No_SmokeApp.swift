069bce519bd31379c91c7eee743a7f6de573a972
Promal <promal.barua.cse@gmail.com>
2023-10-18 16:15:06 +0600

    Initial Commit

---
+++ No Smoke/No_SmokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_SmokeApp.swift
+//  No Smoke
+//
+//  Created by Promal on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct No_SmokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
